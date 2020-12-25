61a95aa94b39e7a72070bed179f16e9b43cd0312
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2020-12-26 02:12:29 +0900

    set grid item

--- src/firebase.js
+++ src/firebase.js
@@ -12,8 +12,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('message');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text }) => {
-  messageRef.push({ name, text });
+  messagesRef.push({ name, text });
 };
